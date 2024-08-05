19d6200b9a29da0224e98f7c0fbc2acaaefb1d47
MAB <muhammadab3321@gmail.com>
2024-08-05 13:16:07 +0500

    Working Now

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import dotenv
 
 def main():
     # dotenv.read_dotenv()
-    """Run administrative tasks."""
+    """Run administrative tasks"""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bizaibo-backend-api.settings')
     try:
         from django.core.management import execute_from_command_line
